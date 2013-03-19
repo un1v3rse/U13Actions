9c1cd204e8000ff0babc6e0ec509be80336927a9
Chris Wright <chris@bywrights.com>
2013-03-19 12:50:01 -0700

    Initial Commit

---
+++ U13Actions/U13AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  U13AppDelegate.h
+//  U13Actions
+//
+//  Created by Chris Wright on 13-03-19.
+//  Copyright (c) 2013 Universe 13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface U13AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ U13ActionsTests/U13ActionsTests.h
@@ -0,0 +1,13 @@
+//
+//  U13ActionsTests.h
+//  U13ActionsTests
+//
+//  Created by Chris Wright on 13-03-19.
+//  Copyright (c) 2013 Universe 13. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface U13ActionsTests : SenTestCase
+
+@end
